733c40823f7a2c857133bfc6f19ab676a379f3c9
Nícolas de Moraes <97860943+Agurdam@users.noreply.github.com>
2023-04-15 00:57:22 -0300

    Delete java/com/Attornatus/TestAttornatus directory

--- java/com/Attornatus/TestAttornatus/repositories/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Attornatus.TestAttornatus.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Attornatus.TestAttornatus.model.PersonModel;
-
-public interface PersonRepository extends JpaRepository<PersonModel, Long> {
-
-}
